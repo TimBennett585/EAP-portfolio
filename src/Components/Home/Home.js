bbfae857066574ef5f136b86a88be705f1c5ea85
Tim <Azacar02@gmail.com>
2024-03-14 12:42:58 -0400

    First commit

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          Erika Pugh, PhD
+        </h1>
+        <p>A brief introduction to your areas of expertise and what visitors can expect to find on your website. </p>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/Components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import Sidebar from "../Sidebar/Sidebar";
+import "./Layout.css";
+
+const Layout = () => {
+  return (
+    <div className="App">
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Layout;
